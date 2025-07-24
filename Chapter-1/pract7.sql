c70b850bd2ffd679b7de6c86def44637773589b2
usmanrajaa <ansariusman858@gmail.com>
2025-07-24 15:35:02 +0530

    sql files added

---
+++ Chapter-1/pract7.sql
@@ -0,0 +1,70 @@
+SHOW DATABASES;
+CREATE DATABASE practice7;
+SHOW TABLES FROM practice7;
+USE practice7;
+DROP TABLE products;
+SELECT * FROM products;
+CREATE TABLE products(
+product_id TINYINT,
+product_no SMALLINT,
+product_price INT,
+product_info BIGINT,
+product_info1 DOUBLE(10,2),
+product_name CHAR(10),
+product_address VARCHAR(50),
+product_manufacturing_details  TEXT,
+product_category ENUM("TECH","NON-TECH","OTHERS"),is_available BOOL,
+created_at DATE,
+created_time TIME,
+created_info DATETIME,
+updated_at TIMESTAMP,
+PRIMARY KEY(product_id,product_no)
+);
+
+CREATE TABLE products1(
+product_id TINYINT,
+product_no SMALLINT,
+product_price INT,
+product_info BIGINT,
+product_info1 DOUBLE(10,2),
+product_name CHAR(10),
+product_address VARCHAR(50),
+product_manufacturing_details  TEXT,
+product_category ENUM("TECH","NON-TECH","OTHERS"),is_available BOOL,
+created_at DATE,
+created_time TIME,
+created_info DATETIME,
+updated_at TIMESTAMP
+);
+DROP TABLE products1;
+SELECT * FROM products1;
+ALTER TABLE products1
+ADD CONSTRAINT PRIMARY KEY (product_id);
+
+INSERT INTO products1
+VALUES
+(34,34,4456,7784321,4567.99,"microsoft","gurugram","manufactured in noida and labeled as gurugram","NON-TECH",TRUE,"2024-07-12","16:10:34","2024-05-23,19:28:45",NOW());
+
+ALTER TABLE products
+ADD
+(is_available BOOL,
+created_at DATE,
+created_time TIME,
+created_info DATETIME,
+updated_at TIMESTAMP);
+
+UPDATE products
+SET
+is_available=FALSE,
+created_at ="2024-07-12",
+created_time="16:10:34",
+created_info="2024-05-23,19:28:45",
+updated_at =NOW()
+WHERE product_name="apple";
+
+SET SQL_SAFE_UPDATES = 1;
+
+SELECT * FROM products;
+INSERT INTO products
+values
+(2,34,4456,7784321,"microsoft","gurugram","manufactured in noida and labeled as gurugram","NON-TECH",TRUE,"2024-07-12","16:10:34","2024-05-23,19:28:45",NOW());
